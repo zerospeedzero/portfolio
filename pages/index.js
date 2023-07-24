9c897a87da8f82d676d476b0f762d9e051baeffc
zerospeedzero <zerospeedzero@gmail.com>
2023-07-24 10:27:30 -0600

    Move three js from _document to index.js page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import GLOBE from 'vanta/dist/vanta.globe.min'
 // import GLOBE from 'vanta/src/vanta.globe'
 import { motion, useScroll } from "framer-motion"
+import Head from "next/head"
 
 export default function Home() {
   const [vantaEffect, setVantaEffect] = useState(null)
@@ -32,6 +33,9 @@ export default function Home() {
   }, [vantaEffect])
   return (
     <>
+      <Head>
+        <script async src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+      </Head>
       <motion.div className='bg-p1 fixed top-20 bottom-0 left-0 right-0 z-[-10]' ref={myRef}
         initial={{opacity:0}} animate={{opacity:0.8}} transition={{duration: 2, delay: 2.8}}
       >
